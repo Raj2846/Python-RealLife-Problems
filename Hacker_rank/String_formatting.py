29f415ece1d509fed5ae339a64f1a25b1f9e4734
Rajkumar <163231551+Raj2846@users.noreply.github.com>
2026-07-19 11:16:57 +0530

    Add new script String_formatting.py for formatting integers in decimal, octal, hexadecimal, and binary

---
+++ Hacker_rank/String_formatting.py
@@ -0,0 +1,37 @@
+"""
+Given an integer, , print the following values for each integer  from  to :
+
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+Function Description
+
+Complete the print_formatted function in the editor below.
+
+print_formatted has the following parameters:
+
+int number: the maximum value to print
+"""
+
+
+
+def print_formatted(number):
+
+    width = len(format(number, 'b'))
+
+    for i in range(1, number + 1):
+
+        decimal = str(i)
+        octal = format(i, 'o')
+        hexa = format(i, 'X')
+        binary = format(i, 'b')
+
+        print(decimal.rjust(width),
+              octal.rjust(width),
+              hexa.rjust(width),
+              binary.rjust(width))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
