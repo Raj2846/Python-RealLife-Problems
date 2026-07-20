cfbc4319a4ae6024f69633c24f4be81de85a3cfc
Rajkumar <163231551+Raj2846@users.noreply.github.com>
2026-07-20 21:33:19 +0530

    Add detailed approach explanation and example usage in Longest_common_prefix.py

--- LeetCode/Longest_common_prefix.py
+++ LeetCode/Longest_common_prefix.py
@@ -4,3 +4,27 @@ Write a function to find the longest common prefix string amongst an array of st
 If there is no common prefix, return an empty string "".
 """
 
+"""
+Approach is like :
+take the shortest string from the list as there is not more to compare then the shortest string
+after that run the loop till the length of the shortest string 
+in the loop run the loop for every string in the list 
+compare the shortest string 1st element with other string 1st element
+break the loop if the values does not matche 
+and return the shortest[:i] i.e till the i value in the loop as after the there is no common letter
+"""
+
+strs = ["flower","flow","flight"]    
+def longest_common_prefix(strs):
+    if not strs:
+        return ""        
+    shortest=min(strs,key=len)
+    for i in range(len(shortest)):
+        for word in strs:
+            if word[i] != shortest[i]:
+                return shortest[:i]
+    return shortest
+
+
+ans=longest_common_prefix(strs)
+print(ans)
