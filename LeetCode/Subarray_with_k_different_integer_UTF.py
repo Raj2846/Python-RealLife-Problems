c9278092fcd9023b5bc042e66918038aeb65ebfe
Rajkumar <163231551+Raj2846@users.noreply.github.com>
2026-08-04 23:22:43 +0530

    Add optimized approach  for counting good subarrays with exactly k different integers

--- LeetCode/Subarray_with_k_different_integer_UTF.py
+++ LeetCode/Subarray_with_k_different_integer_UTF.py
@@ -11,7 +11,7 @@ Input: nums = [1,2,1,2,3], k = 2
 Output: 7
 Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
 """
-
+ads=[1,2,1,2,3];z=2
 nums=[1,2,1,3,4];k=3
 class Solution(object):
     def subarraysWithKDistinct(self, nums, k):
@@ -20,6 +20,27 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        def atMost(nums, k):
+            left = 0
+            count = 0
+            freq = {}
+
+            for right in range(len(nums)):
+
+                freq[nums[right]] = freq.get(nums[right], 0) + 1
+
+                while len(freq) > k:
+                    freq[nums[left]] -= 1
+                    if freq[nums[left]] == 0:
+                        del freq[nums[left]]
+                    left += 1
+                    
+                count += right - left + 1
+            return count
+        return atMost(nums,k) - atMost(nums,k-1)
+    
+s=Solution()
+print(s.subarraysWithKDistinct(ads,z))
         
         
 def brutal_force(nums,k):
