939e0c89b3e6b0d11f4577200b49949df4c28b96
Rajkumar <163231551+Raj2846@users.noreply.github.com>
2026-08-09 18:49:36 +0530

    Refactor log analyzer to improve unique IP tracking and request counting

--- Clg_Work/ClassRoom/Unique_visitor_log_analyzer.py
+++ Clg_Work/ClassRoom/Unique_visitor_log_analyzer.py
@@ -8,7 +8,54 @@ malformed rather than crashing the program. A missing log file must be handled
 with a clear, user-friendly message.
 """
 
-with open("access_log.txt","r") as fp:
-    for i in fp:
-        id,date,time,call,ass=i.strip(" ").split("-")
-        print(time)
+def analyzer_log(filename):
+    unique_ips = set()
+    total_req = 0
+    most_visited_page = 0
+    malformed = 0
+    pages_count = {}
+    first_appereance = {}
+    try:
+        with open(filename, "r") as fp:
+            
+            for line in fp:
+                line = line.strip()
+
+                if not line:
+                    continue
+
+                fields = line.split(" - ")
+                # print(fields)
+
+                # count the wrong data lines
+                if (len(fields)) != 3:
+                    malformed += 1
+                    continue
+
+                ip, time, page = fields
+
+                # we store only unique ip addres
+                unique_ips.add(ip)
+                total_req += 1
+
+                if page not in pages_count:
+                    pages_count[page] = 1
+                    first_appereance[page] = total_req
+                else:
+                    pages_count[page] += 1
+    except FileNotFoundError:
+        print(f"Error : The file '{filename}' was not found ")
+        return   
+
+    print(f"Unique IP addresses: {len(unique_ips)}")
+    print(f"Total requests: {total_req}")
+
+    if pages_count:
+        most_visited_page = max(pages_count, key=lambda page: (
+            pages_count[page], -first_appereance[page]))
+        
+        print(f"Most Visited Pages are {most_visited_page}",
+            f"({pages_count[most_visited_page]} visits)"
+            )
+
+analyzer_log("access_log.txt")
