3c42e2ea9766087689b4bc148c346bc2337923ac
Rajkumar <163231551+Raj2846@users.noreply.github.com>
2026-09-05 17:41:05 +0530

    Add two_sum and threeSumClosest implementations with example usage

--- LeetCode/Two_sum.py
+++ LeetCode/Two_sum.py
@@ -10,8 +10,8 @@ def two_sum(nums,target):
         for j in range(len(nums)):
             if nums[i]+nums[j] == target:
                 return ((i,j))
-ans=two_sum([3,3,4],6)
-print(ans)
+# ans=two_sum([3,3,4],6)
+# print(ans)
 
 #hash mapping
 #enumerate() return the tuple of the index and value in the list
@@ -25,5 +25,21 @@ def twoSum(nums, target):
 
         seen[num] = i
             
-ans=twoSum([3,2,4],6)
-print(ans)
+# ans=twoSum([3,2,4],6)
+# print(ans)
+
+
+def sec_high(nums):
+    max_num=0
+    sec_high_max=0
+    
+    for i in nums:
+        if i > max_num:
+            sec_high_max=max_num
+            max_num=i
+        elif i > sec_high_max and i != max_num:
+            sec_high_max=i
+    return sec_high_max        
+    
+nums=[10, 5, 20, 8, 20, 15]
+print(sec_high(nums))
